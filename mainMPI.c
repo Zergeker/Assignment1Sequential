519b2212a52fc2f6069550dd5f5d6eb2ea3c3add
Zergeker <zergec@yandex.ru>
2022-09-26 02:09:11 +0200

    Sequential string brute force.

---
+++ mainMPI.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "crackme.h"
+
+
+int main(int argc, char** argv) {
+
+	const int alphabet = 127;
+
+	int strSize;
+
+	printf("INSERT VALUE: ");
+	scanf("%d", strSize);
+
+	char* str = (char*)malloc((strSize + 1) * sizeof(char));
+	for (int i = 0; i < strSize; i++)
+	{
+		str[i] = -128;
+	}
+
+	str[strSize] = '\0';
+
+	time_t t1 = time(0);
+
+	while ((int)str[0] <= alphabet)
+	{
+		int s = p(strSize, str);
+		if (s == 1)
+		{
+			time_t t2 = time(0);
+			printf("Password found: %s, time taken: %f seconds\n", str, difftime(t2, t1));
+			free(str);
+			return 0;
+		}
+		
+		str[strSize-1] = (int)str[strSize-1]+1;
+		printf("%d \n", str[strSize-1]);
+		int j = strSize - 1;
+		while ((int)str[j] == alphabet)
+		{
+			if (j > 0)
+			{
+				str[j] = -128;
+				j--;
+				str[j] = (int)str[j] + 1;
+			}
+			else
+			{
+				free(str);
+				return 0;
+			}
+		}
+	}
+	
+	free(str);
+
+	return 0;
+}
